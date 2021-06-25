799f254fa5cc87b289161300f10d80a229824686
dance2die <sungmkim80@gmail.com>
2021-06-24 21:45:52 -0400

    Replaced xregexp with @downwindcss/variant-group

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const postcss = require('postcss');
-const XRegExp = require('xregexp');
+const { parse } = require('@downwindcss/variant-group');
 
 // From Tailwind CSS source code,
 // https://github.com/tailwindlabs/tailwindcss/blob/55653ba0041cf2806f236f00c59307b12f757385/src/lib/substituteClassApplyAtRules.js#L14-L30
@@ -24,50 +24,11 @@ function hasAtRule(css, atRule, condition) {
 	return found;
 }
 
-const newline = /\r?\n|\r|\t/gim;
-// https://stackoverflow.com/a/449000/4035
-const start = '(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)+:\\(';
-// (?!]) is a negative lookahead to check against jit mode 'calc()'
-// https://stackoverflow.com/a/31201710/4035
-const end = '\\)(?!\\])';
-const flags = 'gimu';
-const valueNames = [ 'between', 'variant', 'inside-variant' ];
-
-const matcher = (text) =>
-	XRegExp.matchRecursive(text.replace(newline, ' '), start, end, flags, {
-		valueNames
-	});
-
-// variant with ":"
-function build(text, result = [], variant = '') {
-	const matches = matcher(text);
-	if (variant === '' && matches.length === 1) {
-		result.push(matches[0].value);
-		return;
-	}
-
-	let currentVariant = variant;
-	matches.map(({ name, value }) => {
-		if (name === 'between') {
-			// chain variants (e.g. sm:dark:hover:bg-black)
-			result.push(...value.split(' ').filter(Boolean).map((utility) => `${variant}${utility}`));
-		} else if (name === 'variant') {
-			// remove '(' from 'md:('
-			currentVariant = currentVariant + value.substring(0, value.length - 1);
-		} else if (name === 'inside-variant') {
-			build(value, result, currentVariant);
-			// restore variant
-			currentVariant = variant;
-		}
-	});
-}
-
 function processCSS(css) {
 	css.walkAtRules('apply-group', (atRule) => {
-		const built = [];
+		// const built = [];
 		atRule.name = 'apply';
-		build(atRule.params, built);
-		atRule.params = built.join(' ');
+		atRule.params = parse(atRule.params).join(' ');
 	});
 }
 
